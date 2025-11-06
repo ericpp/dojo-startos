f042623a0a5bc2cdcfbc5bca2bbdb432b89e3371
linkinparkrulz <linkinparkrulz@protonmail.com>
2025-11-06 07:18:19 -0600

    Convert migrations.ts to use double quotes

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,34 +1,34 @@
-import { compat, types as T } from '../deps.ts'
+import { compat, types as T } from "../deps.ts"
 
 export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping(
   {
-    '1.28.0.0': {
+    "1.28.0.0": {
       up: compat.migrations.updateConfig(
         (config: any) => {
           // Add default soroban and pandotx config for users upgrading from versions before 1.28.0
-          config['soroban-announce'] = {
-            enabled: 'disabled',
+          config["soroban-announce"] = {
+            enabled: "disabled",
           }
-          config['pandotx-push'] = true
-          config['pandotx-retries'] = 2
-          config['pandotx-fallback-mode'] = 'convenient'
+          config["pandotx-push"] = true
+          config["pandotx-retries"] = 2
+          config["pandotx-fallback-mode"] = "convenient"
           return config
         },
         true,
-        { version: '1.28.0.0', type: 'up' },
+        { version: "1.28.0.0", type: "up" },
       ),
       down: compat.migrations.updateConfig(
         (config: any) => {
-          delete config['soroban-announce']
-          delete config['pandotx-push']
-          delete config['pandotx-retries']
-          delete config['pandotx-fallback-mode']
+          delete config["soroban-announce"]
+          delete config["pandotx-push"]
+          delete config["pandotx-retries"]
+          delete config["pandotx-fallback-mode"]
           return config
         },
         true,
-        { version: '1.28.0.0', type: 'down' },
+        { version: "1.28.0.0", type: "down" },
       ),
     },
   },
-  '1.28.0.0',
+  "1.28.0.0",
 )
