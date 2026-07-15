b1a3764b7dd2a791136451db6c27d5e1027eb036
Eric P <ericpp@gmail.com>
2026-07-14 21:38:11 -0500

    Bump version

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_1_29_2_0 } from './v1.29.2.0'
+import { v_1_29_2_1 } from './v1.29.2.1'
 
 export const versionGraph = VersionGraph.of({
-  current: v_1_29_2_0,
+  current: v_1_29_2_1,
   other: [],
 })
